81913aee8005b9af4eb62c8e927179951a759553
Albert Su <albertsu2001@gmail.com>
2024-03-03 02:09:56 -0700

    ✅Temp stash

--- deploy/hardhat/001_deployContracts.ts
+++ deploy/hardhat/001_deployContracts.ts
@@ -1,6 +1,10 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import * as WrappedTokenArtifact from "../../artifacts/src/WrappedToken.sol/WrappedToken.json";
+import { ethers } from "ethers";
+import * as EdgelessDepositArtifact from "../../artifacts/src/EdgelessDeposit.sol/EdgelessDeposit.json";
+import * as StakingManagerArtifact from "../../artifacts/src/StakingManager.sol/StakingManager.json";
+import * as EthStrategyArtifact from "../../artifacts/src/strategies/EthStrategy.sol/EthStrategy.json";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
@@ -9,29 +13,39 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const EdgelessDeposit = await getOrNull("EdgelessDeposit");
   if (!EdgelessDeposit) {
+    await deploy("StakingManagerImpl", { from: deployer, log: true, contract: "StakingManager" });
+    const StakingManager = new ethers.Contract((await get("StakingManagerImpl")).address, StakingManagerArtifact.abi);
+    const stakingManagerData = StakingManager.interface.encodeFunctionData("initialize", [deployer]);
     await deploy("StakingManager", {
-      from: deployer, // address that will perform the transaction.
+      from: deployer,
       log: true,
-      proxy: {
-        proxyContract: "OpenZeppelinTransparentProxy", // default to "EIP173Proxy"
-        execute: {
-          methodName: "initialize",
-          args: [deployer],
-        },
-      },
+      contract: "ERC1967Proxy",
+      args: [(await get("StakingManagerImpl")).address, stakingManagerData],
+    });
+    await save("StakingManager", {
+      address: (await get("StakingManager")).address,
+      abi: StakingManagerArtifact["abi"],
     });
 
+    await deploy("EdgelessDepositImpl", { from: deployer, log: true, contract: "EdgelessDeposit" });
+    const EdgelessDeposit = new ethers.Contract(
+      (await get("EdgelessDepositImpl")).address,
+      EdgelessDepositArtifact.abi,
+    );
+    const edgelessDepositData = EdgelessDeposit.interface.encodeFunctionData("initialize", [
+      deployer,
+      l1StandardBridge,
+      (await get("StakingManager")).address,
+    ]);
     await deploy("EdgelessDeposit", {
-      from: deployer, // address that will perform the transaction.
+      from: deployer,
       log: true,
-      proxy: {
-        proxyContract: "OpenZeppelinTransparentProxy", // default to "EIP173Proxy"
-        execute: {
-          methodName: "initialize",
-          args: [deployer, l1StandardBridge, (await get("StakingManager")).address],
-        },
-      },
-      skipIfAlreadyDeployed: true,
+      contract: "ERC1967Proxy",
+      args: [(await get("EdgelessDepositImpl")).address, edgelessDepositData],
+    });
+    await save("EdgelessDeposit", {
+      address: (await get("EdgelessDeposit")).address,
+      abi: EdgelessDepositArtifact["abi"],
     });
 
     await save("Edgeless Wrapped ETH", {
@@ -41,19 +55,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     await execute("StakingManager", { from: deployer, log: true }, "setStaker", (await get("EdgelessDeposit")).address);
 
+    await deploy("EthStrategyImpl", { from: deployer, log: true, contract: "EthStrategy" });
+    const EthStrategy = new ethers.Contract((await get("EthStrategyImpl")).address, EthStrategyArtifact.abi);
+    const ethStrategyData = EthStrategy.interface.encodeFunctionData("initialize", [
+      deployer,
+      (await get("StakingManager")).address,
+    ]);
     await deploy("EthStrategy", {
       from: deployer,
-      proxy: {
-        proxyContract: "OpenZeppelinTransparentProxy",
-        execute: {
-          init: {
-            methodName: "initialize",
-            args: [deployer, (await get("StakingManager")).address],
-          },
-        },
-      },
-      skipIfAlreadyDeployed: true,
       log: true,
+      contract: "ERC1967Proxy",
+      args: [(await get("EthStrategyImpl")).address, ethStrategyData],
     });
 
     await execute(
--- deploy/hardhat/003_upgradeTest.ts
+++ deploy/hardhat/003_upgradeTest.ts
@@ -1,7 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "ethers";
-import * as EdgelessDepositArtifact from "../../artifacts/src/EdgelessDeposit.sol/EdgelessDeposit.json";
 import * as StakingManagerArtifact from "../../artifacts/src/StakingManager.sol/StakingManager.json";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -9,38 +8,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, execute, get, getOrNull, log, read } = deployments;
   const { deployer, l1StandardBridge } = await getNamedAccounts();
 
-  const EdgelessDeposit = await getOrNull("EdgelessDeposit");
-  if (EdgelessDeposit) {
-    await deploy("StakingManagerImpl", {
-      contract: "StakingManager",
-      from: deployer, // address that will perform the transaction.
+  await deploy("s", {
+    contract: "StakingManager",
+    from: deployer, // address that will perform the transaction.
+    log: true,
+  });
+  const StakingManager = new ethers.Contract((await get("StakingManager")).address, StakingManagerArtifact.abi);
+  const stakingManagerData = StakingManager.interface.encodeFunctionData("initialize", [deployer]);
+  console.log(stakingManagerData)
+  await execute(
+    "StakingManager",
+    {
+      from: deployer,
       log: true,
-    });
-    const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-    const signer = new ethers.Wallet("0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d", provider);
-    const StakingManager = new ethers.Contract(
-      (await get("StakingManager")).address,
-      StakingManagerArtifact.abi,
-      signer,
-    );
-    console.log(signer.address, deployer);
-    const stakingManagerData = StakingManager.interface.encodeFunctionData("initialize", [deployer]);
-    await StakingManager.connect(signer).upgradeToAndCall(
-      (await get("StakingManagerImpl")).address,
-      stakingManagerData,
-    );
-    // await execute(
-    //   "StakingManager",
-    //   {
-    //     from: deployer,
-    //     log: true,
-    //   },
-    //   "upgradeToAndCall",
-    //   (await get("StakingManagerImpl")).address,
-    //   stakingManagerData,
-    // );
-  } else {
-    log("EdgelessDeposit already deployed, skipping...");
-  }
+    },
+    "upgradeToAndCall",
+    (await get("s")).address,
+    stakingManagerData,
+  );
 };
 export default func;
